b1a66b8ff73d44d77c980ba6777b28f3621125ec
jacamat <solid@jacamat.com>
2014-04-30 23:03:29 -0700

    workspace markup, carousel and image upload functions

--- lolita.js
+++ lolita.js
@@ -1,7 +1,20 @@
 /** @jsx React.DOM */
+
+var FAKE_ITEMS = [
+  'http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-01.png',
+  'http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-02.png',
+  'http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-03.png',
+  'http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-04.png',
+  'http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-05.png',
+  'http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-06.png',
+  'http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-07.png',
+  'http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-09.png',
+  'http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-10.png'
+];
+
 $.ajaxSetup({
   beforeSend: function(xhr) {
-    xhr.setRequestHeader('Authorization', 'Bearer ZENPQU1BVW96a20=|1429939491|zAKJ+H4ma1/KW9z6b1P7TlDhmb9BvoQ94Xn5HNtOoDI=');
+    xhr.setRequestHeader('Authorization', 'Bearer ZENPQU1Bb1AySmI=|1430295240|wVuklXUsOnYwMHvcZGHvcTvirvlLYPsXwqp/w9iUPG0=');
   }
 });
 
@@ -28,6 +41,29 @@ var Sidebar = React.createClass({
   }
 });
 
+var Carousel = React.createClass({
+  render: function() {
+    return (
+      <div className="carousel">
+        {_.map(this.props.items, this.createCarouselItem)}
+      </div>
+    );
+  },
+  createCarouselItem: function(item) {
+    return (
+      <div className="carousel-item" onClick={this.pickFile}>
+        <img src={item} />
+      </div>
+    );
+  },
+  pickFile: function() {
+    filepicker.setKey('ApjPJ9vSBQVqmLQv3v38gz');
+    filepicker.pick(function(InkBlob){
+      console.log(InkBlob.url);
+    });
+  }
+});
+
 var Lolita = React.createClass({
   getInitialState: function() {
     return {
@@ -39,15 +75,12 @@ var Lolita = React.createClass({
       <div className="lolita">
         <div className="workspace">
           <Sidebar recentThreads={this.state.recentThreads} />
-        </div>
-        <div className="carousel">
-          <p>this is a carousel</p>
-        </div>
-        <div className="carousel">
-        <p>this is a carousel</p>
-        </div>
-        <div className="carousel">
-          <p>this is a carousel</p>
+          <div className="board">
+            <Carousel items={FAKE_ITEMS} />
+            <div className="folder-stats">
+              ...
+            </div>
+          </div>
         </div>
         <div className="user-stats">
           <div className="user-stats-inner">
@@ -57,15 +90,15 @@ var Lolita = React.createClass({
       </div>
     );
   },
-  componentDidMount: function() {
-    var self = this;
+  // componentDidMount: function() {
+  //   var self = this;
 
-    $.get('http://localhost:8000/?url=https://platform.quip.com/1/threads/recent?count=20', function(threads) {
-      self.setState({
-        recentThreads: _.sortBy(threads, function(thread) { return thread.thread.updated_usec * -1 })
-      });
-    });
-  }
+  //   $.get('http://localhost:8000/?url=https://platform.quip.com/1/threads/recent?count=20', function(threads) {
+  //     self.setState({
+  //       recentThreads: _.sortBy(threads, function(thread) { return thread.thread.updated_usec * -1 })
+  //     });
+  //   });
+  // }
 });
 
 React.renderComponent(<Lolita />, document.body);
